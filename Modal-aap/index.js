c4f61c6cc0be3479df32bfef178eca634b902c72
Shivam singh <shivam.singhcs2020@indoreinstitute.com>
2023-03-16 23:19:05 +0530

    modal aap modified

--- Modal-aap/index.js
+++ Modal-aap/index.js
@@ -1,26 +1,41 @@
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+
+// let openModal = document.querySelector("#openModal");
+// let closeModal = document.querySelector("#closeModal");
 
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
-// Modal open function
-const openModal = () =>{
-    console.log("Modal is Open");
-    modal.classList.add("active");
-
-    overlay.classList.add("overlayactive");
-
+//   Modal open function
+const openModal = () => {
+  console.log("Modal is Open");
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
 };
 
-
 // Modal close function
+const closeModal = () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
 
-    const closeModal = () =>
-    {
-        modal.classList.remove("active");
-        overlay.classList.remove("overlayactive");
+// openModal.addEventListener('click' , function(){
+//     console.log("Modal is Open");
+//         modal.classList.add("active");
+    
+//          overlay.classList.add("overlayactive");
 
-    };
+// });
+
+// closeModal.addEventListener('click' , function(){
+//     // console.log("Modal is Open");
+//         modal.classList.remove("active");
+    
+//          overlay.classList.remove("overlayactive");
+
+// });
 
 
 
